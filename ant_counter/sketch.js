b31c4f37386335914fd9b71fe0f911c49af335b7
Roberto Antelo <roberto_antelo@hotmail.com>
2019-03-26 21:18:54 -0600

    Work on Ant walking to a certain point

--- ant_counter/sketch.js
+++ ant_counter/sketch.js
@@ -1,11 +1,15 @@
+const STEP = 2;
+let myAnt;
 function setup() {
   createCanvas(640,640);
   background(245);
+  frameRate(30);
+  myAnt = new Ant(400,300,30,600);
 }
 
 function draw() {
   fill("white");
-  const myAnt = new Ant(400,300,30,600);
+  myAnt.move();
   myAnt.draw();
 }
 
@@ -20,9 +24,7 @@ class Ant {
   }
 
   draw() {
-    const { posx, posy, dirx, diry } = this;
-
-
+ /* Rotation logic
     const a = max(posy, diry) - min(posy, diry);
     const c = dist(posx, posy, dirx, diry);
 
@@ -37,11 +39,31 @@ class Ant {
     stroke("black");
     ellipse(0, 0, w, h);
     pop()
+  */
 
+    const [w,h] = this.dimention;
+    strokeWeight(2)
+    stroke("black");
+    ellipse(this.posx, this.posy, w, h);
 
     strokeWeight(2)
     stroke("black");
-    point(dirx,diry);
+    point(this.dirx,this.diry);
+  }
+
+  move() {
+    // update 1 position closer to final position
+    // decrease the distance in x OR y by one STEP
+
+    if (random() > 0.5) {
+      if (this.posx != this.dirx) {
+        this.posx = this.posx + (Math.sign(this.dirx - this.posx) * STEP);
+      }
+    } else {
+      if (this.posy != this.diry) {
+        this.posy = this.posy + (Math.sign(this.diry - this.posy) * STEP);
+      }
+    }
   }
 }
 
